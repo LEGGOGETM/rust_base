706e2c7518dfb54efaf4d5a88fa383f7b4c770aa
LEGGOGETM <satisfya@naver.com>
2022-03-29 19:35:45 +0900

    rust_first

---
+++ hello_world_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf.d
@@ -0,0 +1,5 @@
+/Users/leggo/Desktop/RUST/0329/hello_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf: src/main.rs
+
+/Users/leggo/Desktop/RUST/0329/hello_cargo/target/debug/deps/hello_cargo-8751c60557c1b3bf.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/leggo/Desktop/RUST/0329/hello_cargo/target/debug/hello_cargo: /Users/leggo/Desktop/RUST/0329/hello_cargo/src/main.rs
