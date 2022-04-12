0305929b2d2af495e51bbdf274695b772aa07752
LEGGOGETM <satisfya@naver.com>
2022-04-12 19:27:47 +0900

    0412_2

--- guess-to-the-secret-number/src/main.rs
+++ guess-to-the-secret-number/src/main.rs
@@ -4,25 +4,35 @@ use std::io;
 
 fn main() {
 
-    println!("Guess the number!");
+    loop {
+        println!("Guess the number!");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(& mut guess)
+            .expect("Failed to read line");
 
-    let secret_number = rand::thread_rng().gen_range(1..50);
+        let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("{}", secret_number);
+        println!("{}", secret_number);
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue
+            };
 
-    println!("You guessed: {}", guess);
+        println !("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(& secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
